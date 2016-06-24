a105bc4e337986700872889a9fe29aeaef29c11a
Jake Crawford <jjc2718@gmail.com>
2016-06-24 11:11:06 -0400

    Initializing repo for random-strings

---
+++ README.md
@@ -0,0 +1,6 @@
+# random-strings
+
+A simple command-line utility to randomly generate strings (or groups of strings) of a given length. Mostly written for my own use in testing solutions to problems at [http://rosalind.info](http://rosalind.info), but the sky is the limit.
+
+Requires `argparse`, `random`, and `string` (all of which are native Python modules).
+
---
+++ random_strings.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Script to generate random DNA strings of a given length, for
+testing purposes
+
+TODO:
+    - add option to auto-generate a FASTA file
+    - add option to provide desired GC-content for DNA
+"""
+import random
+import string
+import argparse
+
+DNA = 1
+RNA = 2
+PROTEIN = 3
+ALPHA = 4
+ALPHANUMERIC = 5
+
+def choose_base_list(list_choice):
+    return {
+        DNA: ['A', 'T', 'G', 'C'],
+        RNA: ['A', 'U', 'G', 'C'],
+        PROTEIN: [c for c in string.ascii_uppercase
+                  if c not in ['B', 'J', 'O', 'U', 'X', 'Z']],
+        ALPHA: string.ascii_uppercase,
+        ALPHANUMERIC: string.ascii_uppercase + string.digits
+    }[list_choice]
+
+def generate_random_string(length, base_list):
+    return ''.join(random.choice(base_list) for _ in xrange(length))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", nargs="?", default=1,
+                        help="Number of strings to generate")
+    parser.add_argument("-l", "--length", nargs="?", default=10,
+                        help="Length of the desired string")
+    parser.add_argument("-t", "--string_type", nargs="?", default=1,
+                        help="1 for DNA, 2 for RNA, 3 for protein,\
+                              4 for alphabet or 5 for alphanumeric. Defaults\
+                              to DNA")
+
+    opts = parser.parse_args()
+    base_list = choose_base_list(int(opts.string_type))
+    print '\n'.join(generate_random_string(int(opts.length), base_list)
+                    for _ in xrange(int(opts.number)))
+
+if __name__ == '__main__':
+    main()
+
